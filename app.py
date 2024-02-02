28af0afba528ec7ea70a6cd53cadcb1a39454f0b
lukecusato07 <lukecusato1@gmail.com>
2024-02-02 18:03:15 -0500

    error fixes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('app.html')
 
 
 
