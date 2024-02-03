80f6c791f6aa7d5ff301c18e1f6b28cddf46b49a
aakashsell <aakashsell@gmail.com>
2024-02-02 21:22:46 -0500

    working on algo

--- algo.py
+++ algo.py
@@ -1,10 +1,19 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
 
+nltk.download('vader_lexicon')
 
-
-
-
-
+def train():
+    return
 
 
 def analyze(ticker, source):
+    sid = SentimentIntensityAnalyzer()
+    pos = 0
+    neg = 0
     
+    ss = sid.polarity_scores(ticker)
+
+    print(ss)
+
+analyze("wow this is bad")
